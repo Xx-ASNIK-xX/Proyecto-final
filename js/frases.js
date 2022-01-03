8e6e3cdfa98436684cf2debf5ea880d258fa4551
Xx-ASNIK-xX <nico_distortion@hotmail.com>
2022-01-03 07:59:46 -0300

    cambios finales

--- js/frases.js
+++ js/frases.js
@@ -36,26 +36,3 @@ function getImg() {
 		document.getElementById("getBtn").disabled = false;
 	}, 1000);
 }
-
-// async function fetchCat () {
-//     let response = await fetch("https://catfact.ninja/fact");
-   
-
-//     if (response.ok == false) {
-//         throw new Error(response.status);
-//     }
-
-//     let cat = await response.json();
-
-//     return cat;
-// }
-
-// fetchCat().then(cat => {
-//     // Recién ahora tengo los datos
-//     console.log(cat);
-
-//     // Mostrar el nombre de la ciudad en el h2
-//     gatos.innerHTML = cat.fact;
-// });
-
-
