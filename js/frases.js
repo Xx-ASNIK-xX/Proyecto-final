27010edc155d922bbc78f8eb957ac167981cf8d8
Xx-ASNIK-xX <nico_distortion@hotmail.com>
2022-01-03 07:11:11 -0300

    final de los cambios

--- js/frases.js
+++ js/frases.js
@@ -1,18 +1,40 @@
-// al hacer click
-document.querySelector('.getFact').onclick = () => {
-    // API consulta
-    fetch('https://catfact.ninja/fact')
-        .then((response) => {
-            return response.json();
-        })
-        .then(data => {
-            let fact = document.createElement('div')
-            fact.className = 'catFact'
-            fact.innerHTML = data.fact;
-            let el = document.getElementsByClassName('factContainer')[0];
-            el.prepend(fact);
+function getRand() {
+	fetch("https://catfact.ninja/fact?max_length=512")
+		.then((response) => {
+			if (!response.ok)
+				return (document.getElementById("cat-fact").innerHTML =
+					"hooo hay un error.");
+			return response.json();
+		})
+		.then(
+			(data) => (document.getElementById("cat-fact").innerHTML = data.fact)
+		);
+        
+}
 
-        })
+function getImg() {
+	fetch("https://api.thecatapi.com/v1/images/search")
+		.then((response) => {
+			if (!response.ok)
+				return document
+					.getElementById("cat-img")
+			return response.json();
+		})
+		.then((data) => {
+			if (data[0].height > 600) {
+				return getImg();
+			}
+			document
+				.getElementById("cat-img")
+				.setAttribute("src", data[0].url.toString());
+
+			getRand();
+		});
+
+	document.getElementById("getBtn").disabled = true;
+	setTimeout(function () {
+		document.getElementById("getBtn").disabled = false;
+	}, 1000);
 }
 
 // async function fetchCat () {
